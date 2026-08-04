2a81f3f29b17d23f2b2e9ce28c6ed2a9d257daaa
Yash Jain <yj3335@nyu.edu>
2026-08-04 18:13:48 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s) : 1}
+        def dfs(index):
+            if index in dp:
+                return dp[index]
+            if s[index] == '0':
+                return 0 
+            
+            res = dfs(index+1)
+            if (index + 1 < len(s) and (s[index] == '1' or s[index] == '2'
+            and s[index+1] in "0123456")):
+                res += dfs(index+2)
+            
+            dp[index] = res
+            return res
+        return dfs(0)
