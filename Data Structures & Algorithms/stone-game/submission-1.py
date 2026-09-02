61ebfcb1e5e0bdeabb7b47bb796d1250e7c642ce
Yash Jain <yj3335@nyu.edu>
2026-09-03 02:34:21 +0530

    Add: stone-game - submission-1

---
+++ Data Structures & Algorithms/stone-game/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {}
+
+        def dfs(l, r):
+            if l > r: 
+                return 0
+            
+            if (l,r) in dp:
+                return dp[(l,r)]
+            
+            even = True if (r-l) % 2 else False
+            left = piles[l] if even else 0 
+            right = piles[r] if even else 0 
+
+            dp[(l,r)] = max(left + dfs(l+1,r), right + dfs(l, r-1))
+            return dp[(l,r)]
+        
+        return dfs(0, len(piles)-1) > sum(piles) // 2
+        
